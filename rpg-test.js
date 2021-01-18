2dcec229ace4edcbaa2d697f30d8479b3657b8d2
Literal Line <chickenfishbird2@gmail.com>
2021-01-18 09:15:21 +0000

    ver

--- rpg-test.js
+++ rpg-test.js
@@ -7,7 +7,7 @@ var RPG_TEST = (function () {
   var canvas = document.createElement('canvas');
   var stage = canvas.getContext('2d');
   var gameSettings = {
-    version: 'v0.1-20210117-2124est',
+    version: 'v0.1-20210118-0414st',
     authors: ['Literal Line'], // in case you mod or whatever
     width: 768,
     height: 432,
