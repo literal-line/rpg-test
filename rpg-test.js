70068224212b6d9d5845ed306b5aec6a96814a11
Literal Line <13988479+literal-line@users.noreply.github.com>
2021-01-31 04:45:16 -0500

    Update rpg-test.js

--- rpg-test.js
+++ rpg-test.js
@@ -7,7 +7,7 @@ var RPG_TEST = (function () {
   var canvas = document.createElement('canvas');
   var stage = canvas.getContext('2d');
   var gameSettings = {
-    version: 'v0.1-20210131-0433est',
+    version: 'v0.1-20210131-0445est',
     authors: ['Literal Line'], // in case you mod or whatever
     width: 768,
     height: 432,
@@ -49,7 +49,7 @@ var RPG_TEST = (function () {
     canvas.style.imageRendering = gameSettings.aa ? 'auto' : 'pixelated';
     canvas.style.imageRendering = gameSettings.aa ? 'auto' : '-moz-crisp-edges';
     stage.imageSmoothingEnabled = gameSettings.aa;
-    stage.textRendering = gameSettings.aa ? 'auto' : 'pixelated';
+    stage.textRendering = 'auto';
   };
 
   var setupAudio = function () { // prevents sfx from interrupting sound in other tabs on certain browsers
