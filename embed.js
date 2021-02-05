0fdf5327885b051bb25a7631f5ed6a12eb8b2685
Literal Line <>
2021-02-04 23:11:17 -0500

    embed game on external site option

---
+++ embed.js
@@ -0,0 +1,20 @@
+function copyString(str) {
+    var textarea = document.createElement('textarea');
+    textarea.textContent = str;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+}
+
+function embedGame() {
+    var iframeCode = '<iframe width="%widthValue%" height="%heightValue%" src="http://rpg-test.quique.gq/?fullscreen"></iframe>';
+    var widthValue = document.getElementById('widthValue').value;
+    var heightValue = document.getElementById('heightValue').value;
+    if (!widthValue) widthValue = 768;
+    if (!heightValue) heightValue = 432;
+    iframeCode = iframeCode.replace('%widthValue%', widthValue);
+    iframeCode = iframeCode.replace('%heightValue%', heightValue);
+    copyString(iframeCode);
+    document.getElementById('copyMsg').style.display = 'block';
+}
